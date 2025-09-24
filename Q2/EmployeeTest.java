cd8f944ceb4fb10788117854111aff0700a1ffcc
PanuwatJR2266 <6830613010@psu.ac.th>
2025-09-24 20:23:44 +0700

    update

---
+++ Q2/EmployeeTest.java
@@ -0,0 +1,28 @@
+package Q2;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Panuwat", "JR", 20000);
+        Employee e2 = new Employee("Jao", "Sorn", 30000);
+
+        System.out.println("monthly before");
+        printEmployeeInfo(e1);
+        printEmployeeInfo(e2);
+        System.out.println("======================================================================");
+
+        e1.raiseSalary(10);
+        e2.raiseSalary(10);
+
+        System.out.println("monthly after 10%");
+        printEmployeeInfo(e1);
+        printEmployeeInfo(e2);
+        System.out.println("======================================================================");
+    }
+
+    public static void printEmployeeInfo(Employee e) {
+        System.out.println(
+                " Name: " + e.getFirstName() + " " + e.getLastName() + " Monthly: " + e.getMonthlySalary() + " bath"
+                        + " Yearly: " + e.getYearlySalary() + " bath");
+    }
+}
--- Q2/EmproyeeTest.java
+++
@@ -1,7 +0,0 @@
-import Q2.Emproyee;
-
-public class EmproyeeTest {
-    public static void main(String[] args) {
-        Emproyee em = new Emproyee(null, null);
-    }
-}
